7e5d66f902b7de8264b4b9a6cc61b029dd9c6c13
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2024-02-19 11:13:11 +0900

    Initial commit

---
+++ src/ts.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "extends": [
+        "yuko",
+    ],
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "comma-dangle": "off",
+        "@typescript-eslint/comma-dangle": [
+            "error",
+            "always-multiline"
+        ],
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "error"
+        ],
+        "@typescript-eslint/no-empty-interface": "off",
+        "semi": "off",
+        "@typescript-eslint/semi": "error",
+    }
+}
