5a4ef139e13eb7760742b75b8daf24a48936983d
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2024-02-19 17:25:20 +0900

    2 spaces indent

--- ts.js
+++ ts.js
@@ -1,23 +1,23 @@
 module.exports = {
-    "extends": [
-        "yuko",
+  "extends": [
+    "yuko",
+  ],
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": [
+      "error",
+      "always-multiline"
     ],
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
-        "@typescript-eslint"
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error"
     ],
-    "rules": {
-        "comma-dangle": "off",
-        "@typescript-eslint/comma-dangle": [
-            "error",
-            "always-multiline"
-        ],
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": [
-            "error"
-        ],
-        "@typescript-eslint/no-empty-interface": "off",
-        "semi": "off",
-        "@typescript-eslint/semi": "error",
-    }
+    "@typescript-eslint/no-empty-interface": "off",
+    "semi": "off",
+    "@typescript-eslint/semi": "error",
+  }
 }
